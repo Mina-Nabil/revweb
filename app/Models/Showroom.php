2076faad122dd3b38e50dacf3c15132895538dd1
Mina Nabil <mina9492@hotmail.com>
2021-11-30 00:53:18 +0200

    adding bankinfo model

--- app/Models/Showroom.php
+++ app/Models/Showroom.php
@@ -203,6 +203,7 @@ class Showroom extends Model
         try {
             DB::transaction(function () {
                 $this->SHRM_BANK_ID = null;
+                $this->load("bankInfo");
                 $this->save();
                 $this->bankInfo->delete();
             });
