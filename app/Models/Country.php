fefb82cbae12d90c147b65de9034bd23b096d572
Mina Nabil <mina9492@hotmail.com>
2021-11-29 19:34:43 +0200

    adding cities

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     public $timestamps = false;
-    protected $table = true;
+    protected $table = "countries";
 
     function cities(){
         return $this->hasMany(City::class, "CITY_CNTR_ID");
