50ae13e465c503ef7ffde93f976274be669c2fdc
Mina Nabil <mina9492@hotmail.com>
2022-01-08 13:27:30 +0200

    adding car with

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -11,7 +11,7 @@ class Car extends Model
     protected $table = "cars";
     public $timestamps = true;
     protected $appends = array('image', 'image_url');
-    // protected $with = ['model', 'model.type'];
+    protected $with = ['model', 'model.type'];
     protected $image;
 
     protected $casts = [
