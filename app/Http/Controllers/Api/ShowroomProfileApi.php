70a31a44d58d99ef9395cc76ada8036fee13237a
Mina Nabil <mina9492@hotmail.com>
2022-01-23 22:24:31 +0200

    Requests API adjustments

--- app/Http/Controllers/Api/ShowroomProfileApi.php
+++ app/Http/Controllers/Api/ShowroomProfileApi.php
@@ -104,12 +104,12 @@ class ShowroomProfileApi extends BaseApiController
     function acceptJoinRequest(Request $request)
     {
         parent::validateRequest($request, [
-            "sellerID" => "required|exists:sellers,id"
+            "joinRequestID" => "required|exists:join_requests,id"
         ]);
         $seller = $request->user();
         $seller->load('showroom');
         if ($seller->showroom->isManager()) {
-            $ret = $seller->showroom->acceptJoinRequest($request->sellerID);
+            $ret = $seller->showroom->acceptJoinRequest($request->joinRequestID);
             if ($ret) {
                 parent::sendResponse(true, "Request Accepted", null, false);
                 $joinRequest = JoinRequest::findOrFail($request->joinRequestID);
