95edc21445d858b3674ae3602761bdd5b8b1c9af
Mina Nabil <mina9492@hotmail.com>
2022-08-27 14:59:54 +0200

    fixing api imports

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use Database\Seeders\BrandsSeeder;
+use Database\Seeders\CarsTestData;
+use Database\Seeders\CarTypes;
+use Database\Seeders\CitiesSeedData;
+use Database\Seeders\users;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,10 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(users::class);
-        // $this->call(CarTypes::class);
-        // $this->call(BrandsSeeder::class);
-        // $this->call(CarsTestData::class);
+        $this->call(users::class);
+        $this->call(CarTypes::class);
+        $this->call(BrandsSeeder::class);
+        $this->call(CarsTestData::class);
         $this->call(CitiesSeedData::class);
    
     }
