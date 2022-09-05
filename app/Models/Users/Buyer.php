e5b540aebe0b5f14b7641e4198a2ceb89234eca2
Mina Nabil <mina9492@hotmail.com>
2022-09-06 01:19:45 +0200

    adding options done

--- app/Models/Users/Buyer.php
+++ app/Models/Users/Buyer.php
@@ -250,13 +250,13 @@ class Buyer extends Authenticatable
 
     function getActiveRequests()
     {
-        $requests = $this->offer_requests()->appends('options')->whereIn("OFRQ_STTS", [OfferRequest::NEW_KEY, OfferRequest::REPLIED_KEY]);
+        $requests = $this->offer_requests()->append('options')->whereIn("OFRQ_STTS", [OfferRequest::NEW_KEY, OfferRequest::REPLIED_KEY]);
         return $requests->get();
     }
 
     function getRequestsHistory()
     {
-        $requests = $this->offer_requests()->appends('options');
+        $requests = $this->offer_requests()->append('options');
         return $requests->simplePaginate(7);
     }
 
