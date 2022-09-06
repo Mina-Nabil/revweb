22fb7e43333f30fcf59af270aefcab93a4500427
Mina Nabil <mina9492@hotmail.com>
2022-09-06 13:05:12 +0200

    adding options done

--- app/Models/Offers/OfferRequest.php
+++ app/Models/Offers/OfferRequest.php
@@ -136,9 +136,10 @@ class OfferRequest extends Model
         $availableOptionIDs = $this->options()->get()->pluck('id')->toArray();
         return ModelAdjustment::join('adjustments_options', 'ADOP_ADJT_ID', '=', 'model_adjustments.id')
             ->whereIn('adjustments_options.id', $availableOptionIDs)
-            ->select('model_adjustments.*')
+            ->select('model_adjustments.*')->distinct()
             ->with(['options' => function ($query) use ($availableOptionIDs) {
-                $query->where('adjustments_options.ADOP_ADJT_ID', '=', $this->id)->whereIn('adjustments_options.id', $availableOptionIDs);
+                $query->where('adjustments_options.ADOP_ADJT_ID', '=', 'model_adjustments.id')
+                ->whereIn('adjustments_options.id', $availableOptionIDs);
             }])->get();
     }
 
