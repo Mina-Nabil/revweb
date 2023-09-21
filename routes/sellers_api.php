4e8eb0bf3065b3ea4199c3cc51872d089c1f0fa9
Mina Nabil <mina9492@hotmail.com>
2023-09-21 20:55:34 +0300

    adding mark requests

--- routes/sellers_api.php
+++ routes/sellers_api.php
@@ -38,9 +38,9 @@ Route::post('/offers/document', [OffersApiController::class, 'addDocument']);
 Route::delete('/offers/document/{$id}', [OffersApiController::class, 'deleteDoc']);
 Route::post('/offers/extra', [OffersApiController::class, 'addExtra']);
 Route::delete('/offers/extra/{$id}', [OffersApiController::class, 'deleteExtra']);
-Route::get('/offers/{$id}', [OffersApiController::class, 'getOffer']);
 Route::get('/offers/{$id}/documents', [OffersApiController::class, 'getOfferDocuments']);
 Route::get('/offers/{$id}/extras', [OffersApiController::class, 'getOfferExtras']);
+Route::get('/offers/{$id}', [OffersApiController::class, 'getOffer']);
 
 //Showroom Management routes
 Route::get('/showroom', [ShowroomProfileApi::class, 'getShowroom']);
